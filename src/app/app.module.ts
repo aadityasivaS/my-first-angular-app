ac9d492f9db675c71d187a0be714a57b49894b46
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-07-22 13:02:22 +0530

    ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
